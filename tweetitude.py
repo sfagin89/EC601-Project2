1bc4aecec7635d8c8a7dd0bf53af53b8e254ddb7
Sara <sfagin@bu.edu>
2021-10-10 23:42:29 -0400

    Currently just shows recent tweet count and rate attitude of a text block

--- tweetitude.py
+++ tweetitude.py
@@ -9,16 +9,17 @@ from google.cloud import language
 twit = input("Please enter the twitter name you want to search a tweet count for: ")
 twitterer = 'from:' + twit
 
-
 # To set your environment variables in your terminal run the following line:
 # export 'TOKEN'='<your_bearer_token>'
 bearer_token = os.environ.get("TOKEN")
 
 search_url = "https://api.twitter.com/2/tweets/counts/recent"
+#search_url = "https://api.twitter.com/2/tweets/"
 
 # Optional params: start_time,end_time,since_id,until_id,next_token,granularity
 #query_params = {'query': 'from:Asmongold','granularity': 'day'}
 query_params = {'query': twitterer,'granularity': 'day'}
+#query_params = {'ids': "ids=1441446902768177158,1441447738978222081",'tweet.fields': "lang,author_id"}
 
 def analyze_text_sentiment(text):
     client = language.LanguageServiceClient()
@@ -44,7 +45,6 @@ def bearer_oauth(r):
     r.headers["User-Agent"] = "v2RecentTweetCountsPython"
     return r
 
-
 def connect_to_endpoint(url, params):
     response = requests.request("GET", search_url, auth=bearer_oauth, params=params)
     #print(response.status_code)
@@ -55,19 +55,19 @@ def connect_to_endpoint(url, params):
 
 def main():
     json_response = connect_to_endpoint(search_url, query_params)
-    print(type(json_response))
-    print(json.dumps(json_response, indent=4, sort_keys=True))
+    #print(type(json_response)) ##Prints <class 'dict'>
+    #print(json.dumps(json_response, indent=4, sort_keys=True))
 
     sample_object = json.dumps(json_response, indent=4, sort_keys=True)
-    print(sample_object)
-    print(type(sample_object))
+    #print(sample_object) ##Prints Formatted JSON Output
+    #print(type(sample_object)) ##Prints <class 'str'>
     data = json.loads(sample_object)
-    print(type(data))
-    print(data)
+    #print(type(data)) ##Prints <class 'dict'>
+    #print(data) ##Prints Flatted JsON dictionary
     print("Counting recent tweets from user: "+twit+"\n")
 
     #fetching data from dictionary
-    print(data.get('meta'))
+    #print(data.get('meta')) ##Prints {'total_tweet_count': 5}
     twit_count = str(data['meta']['total_tweet_count'])
 
     print("The total number of Tweets from "+twit+" this week is "+twit_count)
