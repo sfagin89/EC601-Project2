003a67651d51e62096c2db3bd07b68d56cd439c6
Sara <sfagin@bu.edu>
2021-10-29 03:17:54 -0400

    added comments

--- tweetitude.py
+++ tweetitude.py
@@ -11,7 +11,10 @@ print("All you need to do is enter a twitter username, and the number of most re
 print("Let's get started.")
 
 twit = input("Enter the twitter username you want to search for recent tweets about: ")
-#twitterer = 'from:' + twit
+
+# Used to indicate tweets made by the referenced account
+# twitterer = 'from:' + twit
+# Used to indicate tweets made to the referenced account
 twitterer = 'to:' + twit
 maxTweets = input("What's the maximum number of tweets you want to include in the calculation? (10-100): ")
 if int(maxTweets) > 15:
@@ -21,12 +24,13 @@ if int(maxTweets) > 15:
 # export 'TOKEN'='<your_bearer_token>'
 bearer_token = os.environ.get("TOKEN")
 
-##Used to Search Recent Tweets directed at Specified User
+# Endpoint URL: Accesses the Twitter API for searching recent tweets
 search_url = "https://api.twitter.com/2/tweets/search/recent"
 
-##Used to Search Recent Tweets directed at Specified User
+# Used to Search Recent Tweets directed at Specified User, and sets the max number of tweets to reference.
 query_params = {'query': twitterer, 'max_results': maxTweets}
 
+# Submits text of a pulled tweet to Google NLP, to determine the likely sentiment of the text.
 def analyze_text_sentiment(text):
     client = language.LanguageServiceClient()
     document = language.Document(content=text, type_=language.Document.Type.PLAIN_TEXT)
@@ -34,7 +38,7 @@ def analyze_text_sentiment(text):
     response = client.analyze_sentiment(document=document)
 
     sentiment = response.document_sentiment
-    #print(str(sentiment.score))
+    #print(str(sentiment.score)) # For bug testing
     results = dict(
         text=text,
         score=f"{sentiment.score:.1%}",
@@ -65,11 +69,13 @@ def main():
     json_response = connect_to_endpoint(search_url, query_params)
     overallMetric = 0
 
+    # Passes each pulled tweet to the NLP sentiment analyzer and updated the total score.
     for item in json_response['data']:
         text = item['text'].replace("\n\n", "\n")
         sentiment = analyze_text_sentiment(text)
         overallMetric = overallMetric + (sentiment['scoreINT']*sentiment['magnitudeINT'])
 
+    # Averages the sentiment metric results to give an overall rating.
     overallMetric = (overallMetric/json_response['meta']['result_count'])
     print("Overall Average Sentiment based on the most recent "+str(json_response['meta']['result_count'])+" tweets: "+f"{overallMetric:.1%}")
     if overallMetric >= 0:
