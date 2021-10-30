cc274fe3528ac7a7213e10b3462ae6ff20d39d65
Sara <sfagin@bu.edu>
2021-10-29 20:12:21 -0400

    testing update for proj 3

--- test_OutOfRangeMax.py
+++ test_OutOfRangeMax.py
@@ -1,31 +1,33 @@
 import tweetitude as tt
+from collections import deque
 import pytest
 
-#def test_gtMax(monkeypatch):
-#    monkeypatch.setattr('builtins.input', lambda _: "105")
+def gen_inputs(inputs):
+    def next_input(_):
+        return inputs.popleft()
+    return next_input
+
+#For testing single input with the twitter user hardcoded
+#def test_gtMax(capfd):
+#    tt.input = lambda _: "105"
 #    tt.connect_to_endpoint()
+#    out, err = capfd.readouterr()
+#    assert out == "Sorry, that's outside of the allowed range. The maximum number of tweets has been set back to the default of 10.\n\n"
 
-#    i = input("What's the maximum number of tweets you want to include in the calculation? (10-100): ")
-
-#    assert i == "Sorry, that's outside of the allowed range. The maximum number of tweets has been set back to the default of 10.\n"
-
-def test_gtMax(capfd):
-    tt.input = lambda _: "105"
+def test_gtMax(capfd, monkeypatch):
+    monkeypatch.setitem(__builtins__, 'input', gen_inputs(deque(["asmongold", "105"])))
     tt.connect_to_endpoint()
-    out, err = capfd.readouterr()
-
+    out, _ = capfd.readouterr()
     assert out == "Sorry, that's outside of the allowed range. The maximum number of tweets has been set back to the default of 10.\n\n"
 
-def test_ltMin(capfd):
-    tt.input = lambda _: "5"
+def test_ltMinx(capfd, monkeypatch):
+    monkeypatch.setitem(__builtins__, 'input', gen_inputs(deque(["asmongold", "5"])))
     tt.connect_to_endpoint()
-    out, err = capfd.readouterr()
-
+    out, _ = capfd.readouterr()
     assert out == "Sorry, that's outside of the allowed range. The maximum number of tweets has been set back to the default of 10.\n\n"
 
-def test_crct(capfd):
-    tt.input = lambda _: "50"
+def test_crct(capfd, monkeypatch):
+    monkeypatch.setitem(__builtins__, 'input', gen_inputs(deque(["asmongold", "50"])))
     tt.connect_to_endpoint()
-    out, err = capfd.readouterr()
-
+    out, _ = capfd.readouterr()
     assert out != "Sorry, that's outside of the allowed range. The maximum number of tweets has been set back to the default of 10.\n\n"
